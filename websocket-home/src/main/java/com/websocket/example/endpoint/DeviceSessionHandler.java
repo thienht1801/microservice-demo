c0241cc7617a69f02be754ddce652cdc68422e30
FSOFT.FPT.VN\thienht <thienht@fsoft.com.vn>
2017-03-29 15:46:53 +0700

    commit

---
+++ websocket-home/src/main/java/com/websocket/example/endpoint/DeviceSessionHandler.java
@@ -0,0 +1,120 @@
+package com.websocket.example.endpoint;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.JsonObject;
+import javax.json.spi.JsonProvider;
+import javax.websocket.Session;
+
+import com.websocket.example.model.Device;
+
+@ApplicationScoped
+public class DeviceSessionHandler {
+		
+	private int deviceId = 0;
+	private Set<Device> deviceSet = new HashSet<Device>();
+	private Set<Session> sessionSet = new HashSet<Session>();
+	
+	public DeviceSessionHandler() {
+		super();
+	}
+
+	public void addSession(Session session){
+		this.sessionSet.add(session);
+        for (Device device : deviceSet) {
+            JsonObject addMessage = createAddMessage(device);
+            sendToSession(session, addMessage);
+        }
+	}
+	
+	public void removeSession(Session session){
+		this.sessionSet.remove(session);
+	}
+	
+	public List<Device> getDevices(){
+		return new ArrayList<Device>(deviceSet);
+	}
+	
+	public void addDevice(Device device){
+        device.setId(deviceId);
+        deviceSet.add(device);
+        deviceId++;
+        JsonObject addMessage = createAddMessage(device);
+        sendToAllConnectedSessions(addMessage);		
+	}
+	
+	public void removeDevice(int id){
+        Device device = getDeviceById(id);
+        if (device != null) {
+            deviceSet.remove(device);
+            JsonProvider provider = JsonProvider.provider();
+            JsonObject removeMessage = provider.createObjectBuilder()
+                    .add("action", "remove")
+                    .add("id", id)
+                    .build();
+            sendToAllConnectedSessions(removeMessage);
+        }		
+	}
+	
+	public void toggleDevice(int id){
+        JsonProvider provider = JsonProvider.provider();
+        Device device = getDeviceById(id);
+        if (device != null) {
+            if ("On".equals(device.getStatus())) {
+                device.setStatus("Off");
+            } else {
+                device.setStatus("On");
+            }
+            JsonObject updateDevMessage = provider.createObjectBuilder()
+                    .add("action", "toggle")
+                    .add("id", device.getId())
+                    .add("status", device.getStatus())
+                    .build();
+            sendToAllConnectedSessions(updateDevMessage);
+        }		
+	}
+	
+	private Device getDeviceById(int id){
+        for (Device device : deviceSet) {
+            if (device.getId() == id) {
+                return device;
+            }
+        }
+        return null;
+	}
+	
+	private JsonObject createAddMessage(Device device){
+        JsonProvider provider = JsonProvider.provider();
+        JsonObject addMessage = provider.createObjectBuilder()
+                .add("action", "add")
+                .add("id", device.getId())
+                .add("name", device.getName())
+                .add("type", device.getType())
+                .add("status", device.getStatus())
+                .add("description", device.getDescription())
+                .build();
+        return addMessage;
+	}
+	
+	private void sendToAllConnectedSessions(JsonObject message){
+        for (Session session : sessionSet) {
+            sendToSession(session, message);
+        }
+	}
+	
+	private void sendToSession(Session session, JsonObject message){
+        try {
+            session.getBasicRemote().sendText(message.toString());
+        } catch (IOException ex) {
+            sessionSet.remove(session);
+            Logger.getLogger(DeviceSessionHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }		
+	}
+}
--- websocket-home/src/main/java/com/websocket/example/endpoint/DeviceWebSocketServer.java
+++ websocket-home/src/main/java/com/websocket/example/endpoint/DeviceWebSocketServer.java
@@ -1,33 +1,69 @@
 package com.websocket.example.endpoint;
 
+import java.io.StringReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
-import javax.enterprise.context.ApplicationScoped;
+
+import com.websocket.example.model.Device;
 
 @ApplicationScoped
 @ServerEndpoint("/ws_server")
 public class DeviceWebSocketServer {
+	
+	
+	private DeviceSessionHandler handler = new DeviceSessionHandler();
+	
 	@OnOpen
 	public void open(Session session){
-		
+		handler.addSession(session);
 	}
 	
 	@OnClose
 	public void close(Session session){
-		
+		handler.removeSession(session);
 	}
 
 	@OnError
 	public void onError(Throwable error){
-		
+		Logger.getLogger(DeviceWebSocketServer.class.getName()).log(Level.SEVERE, null, error);
 	}
 	
 	@OnMessage
 	public void handleMessage(String message, Session session){
-		
+
+        try (JsonReader reader = Json.createReader(new StringReader(message))) {
+            JsonObject jsonMessage = reader.readObject();
+
+            if ("add".equals(jsonMessage.getString("action"))) {
+                Device device = new Device();
+                device.setName(jsonMessage.getString("name"));
+                device.setDescription(jsonMessage.getString("description"));
+                device.setType(jsonMessage.getString("type"));
+                device.setStatus("Off");
+                handler.addDevice(device);
+            }
+
+            if ("remove".equals(jsonMessage.getString("action"))) {
+                int id = (int) jsonMessage.getInt("id");
+                handler.removeDevice(id);
+            }
+
+            if ("toggle".equals(jsonMessage.getString("action"))) {
+                int id = (int) jsonMessage.getInt("id");
+                handler.toggleDevice(id);
+            }
+        }		
 	}
 }
